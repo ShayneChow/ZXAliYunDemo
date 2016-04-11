58e972f86659181a0b9d71c94200a935208b069f
Shayne Chow <shayne@weplus.cn>
2016-04-11 20:06:58 +0800

    定制照片选择框的尺寸

--- ZXAliYunDemo/ViewController.m
+++ ZXAliYunDemo/ViewController.m
@@ -8,8 +8,15 @@
 
 #import "ViewController.h"
 #import "AuthorizationStatusHelper.h"
+#import "VPImageCropperViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface ViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+#define CropViewWidth self.view.frame.size.width
+#define CropViewHeight (CropViewWidth * 2 / 3)
+#define CropViewTop (self.view.frame.size.height / 2 - CropViewHeight / 2)
+
+
+@interface ViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate, VPImageCropperDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
@@ -18,7 +25,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _imageView.layer.cornerRadius = 5;
+    _imageView.clipsToBounds = YES;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -48,7 +57,7 @@
 - (void)actionSheetButtonClicked:(NSInteger)buttonIndex {
     UIImagePickerController *picker = [[UIImagePickerController alloc] init];
     picker.delegate = self;
-    picker.allowsEditing = YES;//设置可编辑
+    //picker.allowsEditing = YES;//设置可编辑
     
     if (buttonIndex == 0) {
         //        拍照
@@ -56,33 +65,70 @@
             return;
         }
         picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        NSMutableArray *mediaTypes = [[NSMutableArray alloc] init];
+        [mediaTypes addObject:(__bridge NSString *)kUTTypeImage];
+        picker.mediaTypes = mediaTypes;
     }else if (buttonIndex == 1){
         //        相册
         if (![AuthorizationStatusHelper checkPhotoLibraryAuthorizationStatus]) {
             return;
         }
         picker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+        NSMutableArray *mediaTypes = [[NSMutableArray alloc] init];
+        [mediaTypes addObject:(__bridge NSString *)kUTTypeImage];
+        picker.mediaTypes = mediaTypes;
     }
-    [self presentViewController:picker animated:YES completion:nil];//进入照相界面
+    [self presentViewController:picker animated:YES completion:nil];    // 进入照相界面or照片选择界面
 }
 
 #pragma mark UIImagePickerControllerDelegate
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
-    [picker dismissViewControllerAnimated:YES completion:^{
-        UIImage *editedImage, *originalImage;
-        editedImage = [info objectForKey:UIImagePickerControllerEditedImage];
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    [picker dismissViewControllerAnimated:YES completion:^(){
+//        UIImage *editedImage, *originalImage;
+//        editedImage = [info objectForKey:UIImagePickerControllerEditedImage];
+//        
+//        _imageView.image = editedImage;
         
-        _imageView.image = editedImage;
+        UIImage *originalImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+        //portraitImg = [self imageByScalingToMaxSize:portraitImg];
+        // present the cropper view controller
+        VPImageCropperViewController *imgCropperVC = [[VPImageCropperViewController alloc] initWithImage:originalImage cropFrame:CGRectMake(0, CropViewTop, CropViewWidth, CropViewHeight) limitScaleRatio:3.0];
+        imgCropperVC.delegate = self;
+        [self presentViewController:imgCropperVC animated:YES completion:^{
+            // TO DO
+        }];
+        
         // 保存原图片到相册中
-        if (picker.sourceType == UIImagePickerControllerSourceTypeCamera) {
-            originalImage = [info objectForKey:UIImagePickerControllerOriginalImage];
-            UIImageWriteToSavedPhotosAlbum(originalImage, self, nil, NULL);
-        }
+//        if (picker.sourceType == UIImagePickerControllerSourceTypeCamera) {
+//            originalImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+//            UIImageWriteToSavedPhotosAlbum(originalImage, self, nil, NULL);
+//        }
     }];
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
     [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark VPImageCropperDelegate
+- (void)imageCropper:(VPImageCropperViewController *)cropperViewController didFinished:(UIImage *)editedImage {
+    _imageView.image = editedImage;
+    [cropperViewController dismissViewControllerAnimated:YES completion:^{
+        // TO DO
+    }];
+}
+
+- (void)imageCropperDidCancel:(VPImageCropperViewController *)cropperViewController {
+    [cropperViewController dismissViewControllerAnimated:YES completion:^{
+    }];
+}
+
+#pragma mark - UINavigationControllerDelegate
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
+}
+
+- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    
 }
 
 @end
