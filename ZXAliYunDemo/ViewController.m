8d6c98aee27014bd284470d0685a15826756450a
Shayne Chow <shayne@weplus.cn>
2016-04-11 22:17:16 +0800

    阿里云简单上传

--- ZXAliYunDemo/ViewController.m
+++ ZXAliYunDemo/ViewController.m
@@ -10,11 +10,16 @@
 #import "AuthorizationStatusHelper.h"
 #import "VPImageCropperViewController.h"
 #import <MobileCoreServices/MobileCoreServices.h>
+#import <AliyunOSSiOS/OSSService.h>
 
 #define CropViewWidth self.view.frame.size.width
 #define CropViewHeight (CropViewWidth * 2 / 3)
 #define CropViewTop (self.view.frame.size.height / 2 - CropViewHeight / 2)
 
+NSString * const endPoint = @"http://oss-cn-hangzhou.aliyuncs.com";
+NSString * const bucket = @"wec";
+
+OSSClient * client;
 
 @interface ViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate, VPImageCropperDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
@@ -28,6 +33,8 @@
     
     _imageView.layer.cornerRadius = 5;
     _imageView.clipsToBounds = YES;
+    
+    [self initOSSClient];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -112,6 +119,7 @@
 
 #pragma mark VPImageCropperDelegate
 - (void)imageCropper:(VPImageCropperViewController *)cropperViewController didFinished:(UIImage *)editedImage {
+    [self uploadObjectAsyncWithImage:editedImage];
     _imageView.image = editedImage;
     [cropperViewController dismissViewControllerAnimated:YES completion:^{
         // TO DO
@@ -129,6 +137,88 @@
 
 - (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
     
+}
+
+- (void)initOSSClient {
+    
+    id<OSSCredentialProvider> credential = [[OSSFederationCredentialProvider alloc] initWithFederationTokenGetter:^OSSFederationToken * {
+        NSURL * url = [NSURL URLWithString:@"http://php.weplus.cn/aliyun_sts/"];
+        NSURLRequest * request = [NSURLRequest requestWithURL:url];
+        OSSTaskCompletionSource * tcs = [OSSTaskCompletionSource taskCompletionSource];
+        NSURLSession * session = [NSURLSession sharedSession];
+        NSURLSessionTask * sessionTask = [session dataTaskWithRequest:request
+                                                    completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                        if (error) {
+                                                            [tcs setError:error];
+                                                            return;
+                                                        }
+                                                        [tcs setResult:data];
+                                                    }];
+        [sessionTask resume];
+        [tcs.task waitUntilFinished];
+        if (tcs.task.error) {
+            NSLog(@"get token error: %@", tcs.task.error);
+            return nil;
+        } else {
+            NSDictionary * object = [NSJSONSerialization JSONObjectWithData:tcs.task.result
+                                                                    options:kNilOptions
+                                                                      error:nil];
+            NSLog(@"%@",object);
+            NSDictionary *tokenDict = [object objectForKey:@"Credentials"];
+            OSSFederationToken * token = [OSSFederationToken new];
+            token.tAccessKey = [tokenDict objectForKey:@"AccessKeyId"];
+            token.tSecretKey = [tokenDict objectForKey:@"AccessKeySecret"];
+            token.tToken = [tokenDict objectForKey:@"SecurityToken"];
+            token.expirationTimeInGMTFormat = [tokenDict objectForKey:@"Expiration"];
+            NSLog(@"get token.tAccessKey: %@", token.tAccessKey);
+            return token;
+        }
+    }];
+    
+    
+    OSSClientConfiguration * conf = [OSSClientConfiguration new];
+    conf.maxRetryCount = 2;
+    conf.timeoutIntervalForRequest = 30;
+    conf.timeoutIntervalForResource = 24 * 60 * 60;
+    
+    client = [[OSSClient alloc] initWithEndpoint:endPoint credentialProvider:credential clientConfiguration:conf];
+}
+
+// 异步上传
+- (void)uploadObjectAsyncWithImage:(UIImage *)image {
+    OSSPutObjectRequest * put = [OSSPutObjectRequest new];
+    
+    // required fields
+    put.bucketName = bucket;
+    put.objectKey = @"file-name";
+    
+    //UIImage转换为NSData
+    NSData *imageData = UIImagePNGRepresentation(image);
+    put.uploadingData = imageData; // 直接上传NSData
+    
+//    NSString * docDir = [self getDocumentDirectory];
+//    put.uploadingFileURL = [NSURL fileURLWithPath:[docDir stringByAppendingPathComponent:@"file1m"]];
+    
+    // optional fields
+    put.uploadProgress = ^(int64_t bytesSent, int64_t totalByteSent, int64_t totalBytesExpectedToSend) {
+        NSLog(@"%lld, %lld, %lld", bytesSent, totalByteSent, totalBytesExpectedToSend);
+    };
+    put.contentType = @"";
+    put.contentMd5 = @"";
+    put.contentEncoding = @"";
+    put.contentDisposition = @"";
+    
+    OSSTask * putTask = [client putObject:put];
+    
+    [putTask continueWithBlock:^id(OSSTask *task) {
+        NSLog(@"objectKey: %@", put.objectKey);
+        if (!task.error) {
+            NSLog(@"upload object success!");
+        } else {
+            NSLog(@"upload object failed, error: %@" , task.error);
+        }
+        return nil;
+    }];
 }
 
 @end
